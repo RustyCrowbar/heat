649c37ffdfd8d48dd4eeaffa3257377c19fb066d
Valentin Grimaldi <valentin.grimaldi@epita.fr>
2016-07-22 16:17:37 +0200

    make_gif : optimize background processes for the number of cores

--- samuel0paul/tools/make_gif.bash
+++ samuel0paul/tools/make_gif.bash
@@ -12,17 +12,27 @@ typeset -la data_files=( $(ls | grep "temperature_*") )
 typeset -li i=${#data_files[@]}
 
 mkdir -p "$img_dir"
-
 echo "Plotting matrices..."
+
+cores=$(grep -c ^processor /proc/cpuinfo)
+(( cores *= 2 ))
+
+jobs=0
 
 for (( i = i - 1; i >= 0; i = i - 1 )); do
 	local_file=\'${data_files[$i]}\'
 	local_dir=\'$img_dir\'
+
+        while (( jobs > cores )); do
+            jobs=$(jobs -r | wc -l)
+        done
+
 	gnuplot -e "output_file_suffix=$i; output_dir=$local_dir; input_file=$local_file" \
 		-c tools/plot_heatmap.plt &
+
+        (( ++jobs ))
 done
 
-jobs=1
 while (( jobs > 0 )); do
     jobs=$(jobs -r | wc -l)
 done
